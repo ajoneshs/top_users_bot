b5025aa0d697134e74c4f256992bce3f019f9d47
Andrew Jones <ajoneshs@gmail.com>
2023-01-05 01:59:57 -0500

    added function that adds post/comment ids to lists

--- top_users_bot.py
+++ top_users_bot.py
@@ -5,7 +5,7 @@ import time
 reddit = praw.Reddit("config")
 
 # sets subreddit for bot to run in
-subreddit_name = "progressivedemocrats"
+subreddit_name = "a"
 subreddit = reddit.subreddit(subreddit_name)
 
 # will be populated with the usernames of the top 10 users each month
@@ -14,26 +14,40 @@ top_users = []
 # will be populated with usernames and their participation score
 user_rank = {}
 
-
 # creates the template for the bot to send in its monthly message
-message_template = """Your top users list for r/%(subreddit_name)s from 
-                      %(month)s is ready. \n\n"""
+message_template = ("Your top users list for r/%(subreddit_name)s from "
+                    "%(month)s is ready. \n\n")
 
 # prevents mods from being included in the top user list
 excluded_users = list(subreddit.moderator())
 
 
-'''
-for comment in subreddit.stream.comments():
-    print(comment)
-    print('hello')
+t1 = time.time()
+print(t1)
 
-for submission in subreddit.stream.submissions():
-    print(submission)
-    print('yo')
-'''
+# watches comment/submission streams and adds ids to respective lists
+def monthly_activity():
+    monthly_comments = []
+    monthly_posts = []
+    comment_stream = subreddit.stream.comments(pause_after=-1)
+    submission_stream = subreddit.stream.submissions(pause_after=-1)
+    while time.time() - t1 < 5: # will later replace this with logic that keeps loop running during the active month
+        for comment in comment_stream:
+            if comment is None:
+                break
+            monthly_comments.append(comment.id)
+        for submission in submission_stream:
+            if submission is None:
+                break
+            monthly_posts.append(submission.id)
+    return monthly_comments, monthly_posts
 
-# sample user_rank for testing purposes
+print(monthly_activity())
+print(time.time() - t1)
+
+
+
+# sample user_rank for testing purposes; remove later
 user_rank = {'a': 1, 'c': 3, 'd': 4, 'b': 2, 'f': 6, 'e': 5}
 
 # sorts dict from highest value to lowest
@@ -48,3 +62,9 @@ for user in user_rank:
             top_users.append(user)
     else:
         break
+
+# placeholder; remove later
+month = 'April'
+
+# creates the final message that will be sent
+message = message_template % {'subreddit_name': subreddit_name, 'month': month}
