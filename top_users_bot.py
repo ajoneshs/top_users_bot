a4e5de4edd1bbccc014d367249ca041f7ee05e74
Andrew Jones <ajoneshs@gmail.com>
2023-01-06 23:48:10 -0500

    slight change to outline and started its functions

--- top_users_bot.py
+++ top_users_bot.py
@@ -2,6 +2,15 @@ import praw
 import time
 import datetime
 
+
+# **************Notes**************
+# https://www.reddit.com/r/redditdev/comments/7vj6ox/comment/dtszfzb/
+# **************End****************
+
+# *@ = note on whether or not something works
+# *$ = note on to-do
+
+
 # creates reddit instance using credentials from praw.ini file
 reddit = praw.Reddit("config")
 
@@ -9,54 +18,30 @@ reddit = praw.Reddit("config")
 subreddit_name = "a"
 subreddit = reddit.subreddit(subreddit_name)
 
-# will be populated with the usernames of the top 10 users each month
-top_users = []
-
-# will be populated with usernames and their participation score
-user_rank = {}
-
 # creates the template for the bot to send in its monthly message
 message_template = ("Your top users list for r/%(subreddit_name)s from "
                     "%(month)s is ready. \n\n")
 
-# prevents mods from being included in the top user list
-excluded_users = list(subreddit.moderator())
 
-
-t1 = time.time()
-print(t1)
-
-# https://www.reddit.com/r/redditdev/comments/7vj6ox/comment/dtszfzb/
-# watches comment/submission streams and adds ids to respective lists
-def monthly_activity():
-    untallied_comments = []
-    untallied_posts = []
-    comment_stream = subreddit.stream.comments(pause_after=-1)
-    submission_stream = subreddit.stream.submissions(pause_after=-1)
-    while time.time() - t1 < 5: # will later replace this with logic that keeps loop running during the active month
-        for comment in comment_stream:
-            if comment is None:
-                break
-            untallied_comments.append(comment.id)
-        for submission in submission_stream:
-            if submission is None:
-                break
-            untallied_posts.append(submission.id)
-    return untallied_comments, untallied_posts
-
-print(monthly_activity())
-print(time.time() - t1)
-
-
+# will be populated with usernames and their participation score
+user_rank = {}
 
 # sample user_rank for testing purposes; remove later
 user_rank = {'a': 1, 'c': 3, 'd': 4, 'b': 2, 'f': 6, 'e': 5}
 
-# sorts dict from highest value to lowest
-user_rank = dict(sorted(user_rank.items(), key=lambda item: item[1], reverse=True))
 
 # adds top 10 (or all if total users < 10) users to top_users
+# *@ works
 def top_users(user_rank):
+    top_users = []
+
+    # prevents mods from being included in the top user list
+    # included within function to update as moderators change
+    excluded_users = list(subreddit.moderator())
+
+    # sorts dict from highest value to lowest
+    user_rank = dict(sorted(user_rank.items(), key=lambda item: item[1], reverse=True))
+
     for user in user_rank:
         if len(top_users) < 10 and len(user_rank) != 0:
             if user in excluded_users:
@@ -65,42 +50,59 @@ def top_users(user_rank):
                 top_users.append(user)
         else:
             break
+
     return top_users
 
-# placeholder; remove later
-month = 'April'
 
-# creates the final message that will be sent
-message = message_template % {'subreddit_name': subreddit_name, 'month': month}
-
-
-# sends message to mods for given month with given user rank
-def message_send(month, user_rank):
-    return
+# sends message to mods with current month's top_users
+# currently just prints message instead of sending it
+# *@ should work
+def message_send(top_users):
+    # might want to change message to have rank preceding username (i.e. 1: user1 \n 2: user2...)
+    message = message_template % {'subreddit_name': subreddit_name, 'month': active_month} + '\n'.join(top_users)
+    print(message)
+    # add ability to send this message to mods
 
 
 # goes through comments and posts and adds to user scores if the comments/posts are 12+ hours old
-def tally(comment_list, submission_list):
-    return
+# *@ unfinished
+# *$ write function
+'''
+def tally(untallied_comments, untallied_submissions):
+    for counter, id in enumerate(untallied_comments):
+        if 
+'''
 
 
 untallied_comments = []
-untallied_posts = []
+untallied_submissions = []
 
 comment_stream = subreddit.stream.comments(pause_after=-1)
 submission_stream = subreddit.stream.submissions(pause_after=-1)
+
+active_month = datetime.datetime.now().strftime("%B")
+
 
 
 while True:
 
     # if it's the active month or <= 12 hours into current month
     if (datetime.datetime.now() - datetime.timedelta(hours=12)).strftime("%B") != active_month:
-        top_users()
-        message_send()
-        untallied_comments = []
-        untallied_posts = []
+        # creates list of top 10 users
+        local_top_users = top_users(user_rank)
+        # sends message to mods with top 10 list
+        message_send(local_top_users)
+        # updates active month
+        active_month = datetime.datetime.now().strftime("%B")
 
-    tally(untallied_comments, untallied_posts)
+    # goes through currently untallied comments/submissions and, if it is at
+    # at least 12 hours old, it calculates the participation score of the item
+    # and updates user_rank
+    # untallied_comments and untallied_submissions are updated to remove items
+    # after they have been tallied
+    # remove * after tally() has been finished
+    # *$ finish tally() and remove # on line below
+    # user_rank, untallied_comments, untallied_submissions = tally()
 
     for comment in comment_stream:
         if comment is None:
@@ -110,4 +112,4 @@ while True:
     for submission in submission_stream:
         if submission is None:
             break
-        untallied_posts.append(submission.id)
+        untallied_submissions.append(submission.id)
