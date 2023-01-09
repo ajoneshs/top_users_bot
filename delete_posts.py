ace966ff97662a5e22cb817dbfc66aa6d6c9299d
Andrew Jones <ajoneshs@gmail.com>
2023-01-09 02:29:56 -0500

    adds ability to delete all test users' activity

--- delete_posts.py
+++ delete_posts.py
@@ -1,1 +1,19 @@
 import praw
+
+
+
+reddit_list = [praw.Reddit("test1"), praw.Reddit("test2"), praw.Reddit("test3")]
+
+
+
+subreddit_name = "ajoneshsbottest"
+
+
+for i in reddit_list:
+    reddit = i
+    for submission in reddit.user.me().submissions.new():
+        submission.delete()
+        print(str(submission) + ' was deleted')
+    for comment in reddit.user.me().comments.new():
+        comment.delete()
+        print(str(comment) + ' was deleted')
